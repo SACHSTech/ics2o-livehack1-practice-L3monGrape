7204a3006e48c494ee4d913cfa30e8ea3074e7e1
Andrea Yeh <andrea.yeh23@ycdsbk12.ca>
2021-02-08 15:32:13 +0000

    completed Practice 1

--- f_to_c.py
+++ f_to_c.py
@@ -1,1 +1,18 @@
+"""
+Name: f_to_c.py
 
+Purpose: Lets you enter a measure in degrees Fahrenheit and prints the result in a measure of degrees celsius. 
+
+Author: Yeh. A
+
+Created: 08/02/2021
+
+"""
+#get fahrenheit temperature 
+fahrenheit = int(input("Enter temperature in degrees fahrenheit: ")) 
+
+#calculate conversion
+celsius = (fahrenheit - 32)*5/9
+
+#output result
+print("Temperature in degrees celsius: " + str(celsius))
